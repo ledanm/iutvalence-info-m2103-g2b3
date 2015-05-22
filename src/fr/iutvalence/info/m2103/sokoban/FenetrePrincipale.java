40e2a7b183903528d672d3c69d32576357fe26cc
Maxime <maxime.ledan@iut-valence.fr>
2015-05-22 15:30:08 +0200

    création de la classe FenetrePrincipale : importation de paquetages, permet la mise en place de l'IHM

---
+++ src/fr/iutvalence/info/m2103/sokoban/FenetrePrincipale.java
@@ -0,0 +1,29 @@
+package fr.iutvalence.info.m2103.sokoban;
+import javax.swing.JSplitPane;
+import javax.swing.JLabel;
+import javax.swing.JFrame;
+
+
+
+
+
+/**
+ * IHM de la fenêtre principale
+ * @author litzenbr
+ *
+ */
+public class FenetrePrincipale 
+{
+	/**
+	 * fenêtre contenant l'écran de jeu
+	 */
+	private JFrame fenetre;
+	
+	/**
+	 * méthode pour intialiser l'IHM
+	 */
+	private void initialisationInterfaceGraphique()
+	{
+		this.fenetre= new JFrame();
+	}
+}
